9c91c1272d57836b7ef88d05e23353147f5003b7
RyuChaeHyun <lch010201@gmail.com>
2023-11-27 03:37:14 +0900

    feat: MongoDB 연결 설정

---
+++ src/main/java/com/example/chatService/config/MongoProperties.java
@@ -0,0 +1,14 @@
+package com.example.chatService.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "mongodb")
+public class MongoProperties {
+    String client;
+    String name;
+}
+
