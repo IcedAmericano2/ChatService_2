da6355cecf11923d2c2abc84afa0fbe0d4e8595e
kimyubi <ieieie0419@gmail.co,=m>
2023-12-04 14:11:14 +0900

    Refactor : 설정 파일 수정
    - Notion에 있는 RDS 정보로 수정해도 MySQL 연결 안됨
    - RDS 정보 확인 필요

--- src/main/java/com/example/chatService/domain/ChatRoom.java
+++ src/main/java/com/example/chatService/domain/ChatRoom.java
@@ -3,8 +3,8 @@ package com.example.chatService.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Getter
